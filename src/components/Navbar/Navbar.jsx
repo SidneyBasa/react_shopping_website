0ed2f8ef10be8015474e1ed6fdd3dddf03a8ea7d
Sidney Basa <basa206@hotmail.com>
2023-09-10 21:31:55 -0700

    added links to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 import './Navbar.css'
+import rocket from '../../assets/rocket.png'
+import star from '../../assets/glowing-star.png'
+import idButton from '../../assets/id-button.png'
+import memo from '../../assets/memo.png'
+import order from '../../assets/package.png'
+import lock from '../../assets/locked.png'
+import LinkWithIcon from './LinkWithIcon'
 
 const Navbar = () => {
   return (
@@ -18,8 +25,15 @@ const Navbar = () => {
         </form>
         </div>
 
-        <div className="">
-
+        <div className="align_center navbar_links">
+        <LinkWithIcon title="Home" Link="/" emoji={rocket}/>
+        <LinkWithIcon title="Products" Link="/products" emoji={star}/>
+        <LinkWithIcon title="LogIn" Link="/login" emoji={idButton}/>
+        <LinkWithIcon title="SignUp" Link="/signup" emoji={memo}/>
+        <LinkWithIcon title="My Orders" Link="/myorders" emoji={order}/>
+        <LinkWithIcon title="Logout" Link="/logout" emoji={lock}/>
+            <a href="/cart" className="align_center"></a>
+            Cart <p href="" className="align_center cart_counts">0</p>
         </div>
     </nav>
   )
