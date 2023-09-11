daaa352e2cd8c8ba25f8c69bc0fa7e7f925103db
Sidney Basa <basa206@hotmail.com>
2023-09-10 22:36:26 -0700

    added featured products spacer at landing section

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import iphone from '../../assets/iphone-14-pro.webp';
 import mac from '../../assets/mac-system-cut.jfif';
+import FeaturedProducts from './FeaturedProducts';
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
         Link="/" image={iphone}/>
         {/* Featured Products */}
         {/* Hero section */}
+        <FeaturedProducts />
         <HeroSection 
         title="Build the Ultimate setup" 
         subtitle="You can add Studio Display and color matched magic accessories to your bag after you conficure your mac mini."
