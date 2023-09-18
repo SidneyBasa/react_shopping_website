1d8b7be7aff909cf70b688511d076284ee19b84e
Sidney Basa <basa206@hotmail.com>
2023-09-17 21:14:16 -0700

    Usage of react form hook to handle forms with validations

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,41 +1,74 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
 
 import "./LoginPage.css"
 
 const LoginPage = () => {
-    const nameRef = useRef(null)
-    const phoneRef = useRef(null)
+    // const nameRef = useRef(null)
+    // const phoneRef = useRef(null)
+    const {register, handleSubmit, formState: {errors}} = useForm()
+    // console.log(formState.errors)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const user = {name:"", phone: 0}
-        user.name = (nameRef.current.value);
-        user.phone = parseInt(phoneRef.current.value);
-        console.log(user)
-    }
+    const onSubmit = (formData) => console.log(formData)
+    // console.log(register("name"));
+    // const [user, setUser] = useState({
+    //     name: "",
+    //     phone: ""
+    // })
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const user = {name:"", phone: 0}
+    //     user.name = (nameRef.current.value);
+    //     user.phone = parseInt(phoneRef.current.value);
+    //     console.log(user)
+    // }
 
   return (
     <section className="align_center form_page">
-        <form action="" className="authentication_form" onSubmit={handleSubmit}>
+        <form action="" className="authentication_form" onSubmit={handleSubmit(
+            onSubmit
+            // formData => console.log(formData)
+        )}>
             <h2>Login Form</h2>
             <div className="form_inputs">
                 <div className="">
                     <label htmlFor="name" className="">Name</label>
                     <input 
                     type="text" 
-                    ref={nameRef}
+                    // ref={nameRef}
                     id='name'
                     className="form_text_input" 
-                    placeholder="Enter your name"/>
+                    placeholder="Enter your name"
+                    // onChange={e => setUser({
+                    //     ...user,
+                    //     name: e.target.value
+                    // })}
+                    // value={user.name}
+                    {...register("name", {
+                        required: true,
+                        minLength: 3
+                    })}
+                    />
+                    {/* Usage of optional chaining with ?. syntax */}
+                    {errors.name?.type === "required" &&(<em className="form_error">Please enter your name</em>)}
+                    {errors.name?.type === "minLength" &&(<em className="form_error">Name should be 3 or more characters</em>)}
                 </div>
                 <div className="">
                     <label htmlFor="phone" className="">Phone Number</label>
                     <input 
                     type="number" 
-                    ref={phoneRef}
+                    // ref={phoneRef}
                     id='phone'
                     className="form_text_input" 
-                    placeholder="Enter your phone number"/>
+                    placeholder="Enter your phone number"
+                    // onChange={e => setUser({
+                    //     ...user,
+                    //     phone: parseInt(e.target.value)
+                    // })}
+                    // value={user.phone}
+                    {...register("phone", {valueAsNumber: true})}
+                    />
                    
                 </div>
                 <button type="submit" className="search_button form_submit">Submit</button>
